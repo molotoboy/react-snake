4d316b680c8f502a34a3a675b894661cbbfad2b5
molotoboy <molotoboy@gmail.com>
2022-02-01 11:14:09 +0300

    rename constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,12 @@
 export const CANVAS_SIZE = { x: 800, y: 800 };
-export const SNAKE_START = [];
+export const SNAKE_START = [
+  { x: 9, y: 14 },
+  { x: 9, y: 15 },
+];
+export const SNAKE_BODY_START = [70, 60];
 export const DIRECTION_START = { x: 0, y: -1 };
 export const SCALE = 40;
-export const INITIAL_DELAY = 400;
+export const DELAY_START = 400;
 export const DIRECTIONS = {
   ArrowUp: { x: 0, y: -1 },
   ArrowDown: { x: 0, y: 1 },
